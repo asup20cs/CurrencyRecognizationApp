2aa4bc6ddc23f98626728ab5cd0ad60e4e13c8c9
Ashutosh Khanal Upadhyay <89827229+asup20cs@users.noreply.github.com>
2024-03-22 10:02:54 +0530

    chnages few lines

--- convert.py
+++ convert.py
@@ -1,7 +1,5 @@
 from currency_converter import CurrencyConverter
 cc=CurrencyConverter()
 def convert_currency(amount, from_currency, to_currency):
-    return cc.convert(amount, from_currency, to_currency)
-print(convert_currency(100, 'INR', 'USD'))
-
-
+    amount = cc.convert(amount, from_currency, to_currency)
+    return amount
