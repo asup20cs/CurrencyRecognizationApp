c0c4a7a3633d85ebd668861a03351dde2e69ee16
Ashutosh Khanal Upadhyay <89827229+asup20cs@users.noreply.github.com>
2024-04-20 10:13:43 +0530

    rewriting conersion file

--- convert.py
+++
@@ -1,5 +0,0 @@
-from currency_converter import CurrencyConverter
-cc=CurrencyConverter()
-def convert_currency(amount, from_currency, to_currency):
-    amount = cc.convert(amount, from_currency, to_currency)
-    return amount
